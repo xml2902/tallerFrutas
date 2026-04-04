ce1b75812a41e0180f0d15e62d1ea269d0d6e640
Dani2902 <angiedaniela@users.noreply.github.com>
2026-04-04 00:24:42 -0500

    2. Escena 2: Nivel 1 - Recolección Libre
    Se organizo todo con las misiones en JSON

--- TallerCGFrutas/Assets/Game/Scripts/GameManager.cs
+++ TallerCGFrutas/Assets/Game/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 
     private float globalTime;
 
+    // Contadores para puntacion de frutas
     private int totalApple;
     private int totalOrange;
     private int totalBanana;
@@ -16,10 +17,19 @@ public class GameManager : MonoBehaviour
     private int totalPineapple;
     private int totalStrawberry;
 
+    // Contadores para cantidad de frutas
+    private int countApple;
+    private int countOrange;
+    private int countBanana;
+    private int countKiwi;
+    private int countCherries;
+    private int countMelon;
+    private int countPineapple;
+    private int countStrawberry;
+
 
     void Awake()
     {
-        
         if(Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -27,14 +37,18 @@ public class GameManager : MonoBehaviour
         }
             
             Instance = this;
-            DontDestroyOnLoad(gameObject);
-          
+            DontDestroyOnLoad(gameObject); 
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         globalTime = 0;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ReiniciarContadores();
+        }
     }
 
     // Update is called once per frame
@@ -50,47 +64,72 @@ public class GameManager : MonoBehaviour
 
     public void AddItem(ItemData item)
     {
-        switch(item.itemType)
-        {
-            //case ItemType.Apple:
-            //    totalApples += item.itemValue;
-            //    break;
-            //case ItemType.Orange:
-            //    totalOranges += item.itemValue;
-            //    break;
-        }
+        //switch(item.itemType)
+        //{
+        //    //case ItemType.Apple:
+        //    //    totalApples += item.itemValue;
+        //    //    break;
+        //    //case ItemType.Orange:
+        //    //    totalOranges += item.itemValue;
+        //    //    break;
+        //}
+    }
+
+    public void ReiniciarContadores()
+    {
+        totalApple = 0;
+        totalOrange = 0;
+        totalBanana = 0;
+        totalKiwi = 0;
+        totalCherries = 0;
+        countApple = countOrange = countBanana = countKiwi = countCherries = countMelon = countPineapple = countStrawberry = 0;
+        Debug.Log("Todos los contadores y conteos reiniciados para nuevo nivel.");
     }
 
     public void SumarItem (string itemName, int itemValue)
     {
-        switch(itemName)
+        switch(itemName.ToLower())
         {
-            case "Apple":
+            case "apple":
                 totalApple += itemValue;
+                countApple++;
                 break;
-            case "Orange":
+            case "orange":
                 totalOrange += itemValue;
+                countOrange++;
                 break;
-            case "Banana":
+            case "bananas":
                 totalBanana += itemValue;
+                countBanana++;
                 break;
-            case "Kiwi":
+            case "kiwi":
                 totalKiwi += itemValue;
+                countKiwi++;
                 break;
-            case "Cherries":
+            case "cherries":
                 totalCherries += itemValue;
+                countCherries++;
                 break;
-            case "Melon":
+            case "melon":
                 totalMelon += itemValue;
+                countMelon++;
                 break;
-            case "Pineapple":
+            case "pineapple":
                 totalPineapple += itemValue;
+                countPineapple++;
                 break;
-            case "Strawberry":
+            case "strawberry":
                 totalStrawberry += itemValue;
+                countStrawberry++;
                 break;
         }
     }
+
+    public int CountApple => countApple;
+    public int CountBanana => countBanana;
+    public int CountOrange => countOrange;
+    public int CountKiwi => countKiwi;
+    public int CountCherries => countCherries;
 
     public float GlobalTime { get => globalTime; set => globalTime = value; }
     public int TotalApple { get => totalApple; set => totalApple = value; }
--- TallerCGFrutas/Assets/Game/Scripts/LogicaJson/GameDataLoader.cs
+++ TallerCGFrutas/Assets/Game/Scripts/LogicaJson/GameDataLoader.cs
@@ -2,15 +2,11 @@ using System;
 using System.IO;
 using UnityEngine;
 
-
-
-
 public class GameDataLoader : MonoBehaviour
 {
 
     [SerializeField] private string FileName = "GameData.json";
     public GameData gameData;
-
 
     private void Awake()
     {
@@ -20,26 +16,32 @@ public class GameDataLoader : MonoBehaviour
     private void LoadGameData()
     {
         string path = Path.Combine(Application.streamingAssetsPath, FileName);
-        if (!File.Exists(path)) 
-        
-     
-        
-            {   Debug.LogError($"loading game data from: {path}"+ "Error");
+        if (!File.Exists(path))
+        {
+            Debug.LogError($"loading game data from: {path}" + "Error");
 
             return;
-        
+
         }
 
-        string json= File.ReadAllText(path);
+        string json = File.ReadAllText(path);
         gameData = JsonUtility.FromJson<GameData>(json);
+        Debug.Log("JSON Cargado correctamente.");
 
+    }
 
-        
+    public MisionData ObtenerMisionPorId(int id)
+    {
+        if (gameData == null || gameData.misiones == null) return null;
 
+        foreach (var mision in gameData.misiones)
+        {
+            if (mision.id == id) return mision;
+        }
+        return null;
+    }
 
-}
-
-public ColeccionableData ObtenerColeccionablePorNombre(string nombre)
+    public ColeccionableData ObtenerColeccionablePorNombre(string nombre)
     {
         if (gameData == null || gameData.coleccionables == null) return null;
 
@@ -52,10 +54,24 @@ public ColeccionableData ObtenerColeccionablePorNombre(string nombre)
         return null;
     }
 
+    public void ConfigurarEscena(int misionId, MissionManager manager)
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.TotalApple = 0;
+            GameManager.Instance.TotalOrange = 0;
+            GameManager.Instance.TotalBanana = 0;
+            GameManager.Instance.TotalCherries = 0;
+            GameManager.Instance.TotalKiwi = 0;
+            Debug.Log("GameManager Reiniciado por el Loader.");
+        }
 
-
-
-
+        MisionData mision = ObtenerMisionPorId(misionId);
+        if (mision != null && manager != null)
+        {
+            manager.AsignarMision(mision);
+        }
+    } 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
